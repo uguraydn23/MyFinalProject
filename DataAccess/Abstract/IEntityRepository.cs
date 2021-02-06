ac305b7c87c133ca7d4c73d30394a9d1dc3aadaa
Uğur <uguraydn23@gmail.com>
2021-02-06 14:58:50 +0300

    Added Entity and move to entity

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //generic constraint
+    // class: referans tip
+    // Ya Ientitiy olabilir yada ondan referans alan olabilir
+    // new lene bilir olmalı
+    public interface IEntityRepository<T> where T:class, IEntity, new()
     {
         //filter null , filtrede vermeyebilirsin.
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
