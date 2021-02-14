6e7289dc179b8c813ac968d3df19843c5ddaa6d6
Uğur <uguraydn23@gmail.com>
2021-02-14 20:40:13 +0300

    Added new code lines

---
+++ Entities/DTOs/ProductDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    //Tek başına tablo değil o yüzden IEntity değil . Bir kaç tablonun birleşimi olabilir o yüzden IDto
+    public class ProductDetailDto: IDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+        public short UnitsInStock { get; set; }
+
+    }
+}
