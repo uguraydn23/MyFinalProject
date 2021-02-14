922a6fe067c7c4a5521d16d0604c88f35a6ab6ee
Uğur <uguraydn23@gmail.com>
2021-02-14 22:35:53 +0300

    Added new codeline

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool success, string message):base(success,message)
+        {
+
+        }
+
+        // T türünde data gönder , ve aynı zamanda bu işlemin sonucunu gönder , base ede implemente edelim ,gönderelim
+        public DataResult(T data, bool success):base(success)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+
+    }
+}
